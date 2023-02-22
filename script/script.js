91aca836c325e840c1eb513bc72141660932ea7a
DUBOIS Maxime <maxime.dubois.&@cpe.fr>
2023-02-22 17:43:40 +0100

    table

--- script/script.js
+++ script/script.js
@@ -3,22 +3,22 @@ $(function() {
     $("footer").load("footer.html");
 });
 
-let voyages = {
+const voyages = {
     "japon": {
-        "destination": "Le Japon",
-        "description": "Le japon est beau",
-        "images": ["assets/img/japon/beach.jpg", "assets/img/japon/buildings.jpg", "assets/img/japon/fuji.jpg", "assets/img/japon/temple.jpg"],
-        "prix-nuit": 400,
-        "prix-petitdej": 15,
-        "Annimaux": false,
+        destination: "Le Japon",
+        description: "Le japon est beau",
+        images: ["assets/img/japon/beach.jpg", "assets/img/japon/buildings.jpg", "assets/img/japon/fuji.jpg", "assets/img/japon/temple.jpg"],
+        prixnuit: 400,
+        prixdej: 15,
+        Annimaux: false,
     },
     "maldive": {
-        "destination": "Les Maldives",
-        "description": "Les maldive y'a de l'eau",
-        "images": ["assets/img/maldive/01.avif", "assets/img/maldive/02.avif", "assets/img/maldive/03.avif"],
-        "prix-nuit": 300,
-        "prix-petitdej": 15,
-        "animaux": true,
+        destination: "Les Maldives",
+        description: "Les maldive y'a de l'eau",
+        images: ["assets/img/maldive/01.avif", "assets/img/maldive/02.avif", "assets/img/maldive/03.avif"],
+        prixnuit: 300,
+        prixdej: 15,
+        animaux: true,
     }
 }
 
@@ -28,9 +28,9 @@ window.onload = () => {
     document.getElementById("form").addEventListener('change', changeform);
 
     if (urlParams.get('selection'))
-        var selection = urlParams.get('selection');
+        selection = urlParams.get('selection');
     else
-        var selection = "japon";
+        selection = "japon";
 
     console.log(voyages[selection].destination);
     document.getElementById("titre-destination").innerHTML = voyages[selection].destination;
@@ -40,16 +40,60 @@ window.onload = () => {
         document.getElementById("animaux-destination").innerHTML = document.getElementById("animaux-destination").innerHTML + "Les animaux sont acceptés !";
     else
         document.getElementById("animaux-destination").innerHTML = document.getElementById("animaux-destination").innerHTML + "Les animaux ne sont pas acceptés désolé !";
-
 }
 
 function changeform() {
     console.log('Onchangeform');
     let form = document.getElementById("form");
     let solde = document.getElementById('solde-destination');
-    let datedebut = form.getElementById('date-debut').value;
-    let datefin = form.getElementById('date-fin').value;
-    console.log(datedebut);
-    console.log(datefin);
-    let nbrjour = datefin - datedebut;
+    let datedebut = new Date(document.getElementById('date-debut').value);
+    let datefin = new Date(document.getElementById('date-fin').value);
+    let nbrjour = dateDiff(datedebut, datefin).day;
+    console.log(nbrjour);
+    let nbadulte = document.getElementById('nb-adulte').value;
+    console.log("nb adulte " + nbadulte);
+    let nbenfant = document.getElementById('nb-enfant').value;
+    console.log("nbenfant " + nbenfant);
+
+    let animaux = document.getElementById('animaux-form').checked;
+    console.log("animaux " + animaux);
+    let petitdej = document.getElementById('petitdej').checked;
+    console.log("petitdej " + petitdej);
+
+    let prixnuit = voyages[selection].prixnuit;
+    let prixdej = voyages[selection].prixdej;
+    console.log(prixnuit);
+
+    addligne(["n", "h", "j"]);
+
 };
+
+function addligne(ligne) {
+    var row = document.createElement("tr");
+    for (txt of ligne) {
+        var cell = document.createElement("td");
+        text = document.createTextNode(txt);
+        cell.appendChild(text);
+        row.appendChild(cell);
+    }
+    document.getElementById('solde-destination').appendChild(row);
+}
+
+function dateDiff(date1, date2) {
+    var diff = {} // Initialisation du retour
+    var tmp = date2 - date1;
+
+    tmp = Math.floor(tmp / 1000); // Nombre de secondes entre les 2 dates
+    diff.sec = tmp % 60; // Extraction du nombre de secondes
+
+    tmp = Math.floor((tmp - diff.sec) / 60); // Nombre de minutes (partie entière)
+    diff.min = tmp % 60; // Extraction du nombre de minutes
+
+    tmp = Math.floor((tmp - diff.min) / 60); // Nombre d'heures (entières)
+    diff.hour = tmp % 24; // Extraction du nombre d'heures
+
+    tmp = Math.floor((tmp - diff.hour) / 24); // Nombre de jours restants
+    diff.day = tmp;
+
+    return diff;
+}
