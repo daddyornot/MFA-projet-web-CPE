2d472e67c1fc09bdc4e4841c0f96976e712a5804
I52174 <damien.mailhebiau@edf.fr>
2023-03-01 10:17:26 +0100

    feat: add panier styles

--- script/panier.js
+++ script/panier.js
@@ -5,11 +5,11 @@ window.onload = () => {
     console.log(panier.get());
 
     if (!panier.get()){
-        document.getElementById("liste-destinations").innerHTML = "Aucun Voyage ajouté au panier";
+        document.getElementById("contenu-panier").innerHTML = "Aucun Voyage ajouté au panier";
     }
     else {
         if (panier.get().length == 0)
-            document.getElementById("liste-destinations").innerHTML = "Aucun Voyage ajouté au panier";
+            document.getElementById("contenu-panier").innerHTML = "Aucun Voyage ajouté au panier";
         else
         creationtableau();
     }
@@ -27,14 +27,16 @@ function creationtableau(){
         const voyage = new Reservation(e._selection);
         voyage.all(e._datedebut, e._datefin, e._nbadulte, e._nbenfant, e._petitdej, e._id);
         if(voyage.petitDej)
-            var dej = "Oui";
+            var dej = "Pti dej' inclus";
         else
-            var dej = "Non";
+            var dej = "Pas de Pti dej'";
 
         let clone = document.importNode(template.content, true);
 
         newDestination = clone.firstElementChild.innerHTML
             .replace(/{{destination}}/g, voyage.destination)
+            .replace(/{{dateDebut}}/g, toFormattedDate(voyage.datedebut))
+            .replace(/{{dateFin}}/g, toFormattedDate(voyage.datefin))
             .replace(/{{nbAdultes}}/g, voyage.nbAdulte)
             .replace(/{{nbEnfants}}/g, voyage.nbEnfant)
             .replace(/{{imgDest}}/g, voyage.images[0])
@@ -42,6 +44,14 @@ function creationtableau(){
             .replace(/{{prix}}/g, voyage.total)
             .replace(/{{suppr}}/g, voyage.id);
         clone.firstElementChild.innerHTML = newDestination;
-        document.getElementById("liste-destinations").appendChild(clone);
+        document.getElementById("contenu-panier").appendChild(clone);
     }
+}
+
+function toFormattedDate(date) {
+    return date.toLocaleString("fr-FR", {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    })
 }
