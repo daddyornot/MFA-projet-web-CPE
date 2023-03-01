d52548cf3e2b4b3016d5725bd319103eb02bb0fa
I52174 <damien.mailhebiau@edf.fr>
2023-03-01 16:55:52 +0100

    feat: start adding footer + refactor

--- script/header.js
+++ script/header.js
@@ -1,13 +1,13 @@
 
-    url = window.location.pathname;
-    switch (true) {
-        case url.includes("index"):
-            document.getElementById("nos-voyages").className += "active";
-            break;
-        case url.includes("panier"):
-            document.getElementById("panier").className += "active";
-            break;
-        case url.includes("propos"):
-            document.getElementById("a-propos").className += "active";
-            break;
-    }
+url = window.location.pathname;
+switch (true) {
+    case url.includes("index"):
+        document.getElementById("nos-voyages").className += "active";
+        break;
+    case url.includes("panier"):
+        document.getElementById("panier").className += "active";
+        break;
+    case url.includes("propos"):
+        document.getElementById("a-propos").className += "active";
+        break;
+}
