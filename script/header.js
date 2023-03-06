4b89ebec538b058b9a53862b77f7b21997836b0f
I52174 <damien.mailhebiau@edf.fr>
2023-03-06 11:57:05 +0100

    feat: playpause button

--- script/header.js
+++ script/header.js
@@ -11,3 +11,19 @@ switch (true) {
         document.getElementById("a-propos").className += "active";
         break;
 }
+
+let toggleBackgroundAnimation = document.getElementById("button-background");
+toggleBackgroundAnimation.addEventListener("click", () => {
+    if (toggleBackgroundAnimation.className === "stop-background") {
+        clearInterval(backgroundInterval);
+        toggleBackgroundAnimation.className = "play-background";
+        toggleBackgroundAnimation.getElementsByTagName("span")[0].innerHTML = "play_arrow";
+    }
+    else {
+        randomizeBackground();
+        toggleBackgroundAnimation.className = "stop-background";
+        toggleBackgroundAnimation.getElementsByTagName("span")[0].innerHTML = "stop";
+    }
+
+
+})
