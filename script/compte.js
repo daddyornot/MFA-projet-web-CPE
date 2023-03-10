df1b24176959335518c5c49e9791df463a7495e7
I52174 <damien.mailhebiau@edf.fr>
2023-03-10 13:07:55 +0100

    feat: add users infos

--- script/compte.js
+++ script/compte.js
@@ -4,4 +4,9 @@ let connectedUser = verifUserConnected();
 if (connectedUser) {
     document.getElementById("nom").innerHTML = connectedUser.nom;
     document.getElementById("prenom").innerHTML = connectedUser.prenom;
+    document.getElementById("adresse").innerHTML = connectedUser.adresse;
+    document.getElementById("code-postal").innerHTML = connectedUser.codePostal;
+    document.getElementById("ville").innerHTML = connectedUser.ville;
+    document.getElementById("pays").innerHTML = connectedUser.pays;
+    document.getElementById("tel").innerHTML = connectedUser.tel;
 }
